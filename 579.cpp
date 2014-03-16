137a441cf42efe02831882cf029dcc011e5b2f35
milnex <itsmings@gmail.com>
2014-03-16 01:04:24 -0400

    579 solved

---
+++ 579.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cmath>
+int main()
+{
+	int H, M;
+	while (scanf("%d:%d", &H, &M) == 2 && (H || M)) {
+		float angle = fabs(30*H - 5.5*M);
+		float cangle = 360 - angle;
+		printf("%.3f\n", angle < cangle ? angle : cangle);
+	}
+	return 0;
+}

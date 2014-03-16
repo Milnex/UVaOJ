324be129e1447264b4cb650c5881751d7c2a0b1d
milnex <itsmings@gmail.com>
2014-03-16 01:17:15 -0400

    591 solved

---
+++ 591.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#define NMAX  60
+int main()
+{
+	int n, k, num[NMAX];
+	for (int i = 1; scanf("%d", &n) && n; i++) {
+		int avg = 0, moves = 0;
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &num[j]);
+			avg += num[j];
+		}
+		avg /= n;
+		for (int j = 0; j < n; j++) 
+			if (num[j] < avg)
+				moves += avg - num[j];
+		printf("Set #%d\nThe minimum number of moves is %d.\n\n", i, moves);
+	}
+}
